3e8901b0a689dff1281b1cb1063c2693ecce3b63
suzumiya1215 <1030011936@qq.com>
2017-09-17 12:54:37 +0800

    STL冒泡排序

---
+++ STL冒泡排序.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<iterator>
+#include<string>
+#include<vector>
+#include<set>
+#include<stack>
+#include<algorithm>
+#include <sstream>
+using namespace std;
+
+vector<int> Bubble_Sort(vector<int>& nums)//冒泡排序失败
+{
+
+	bool again = false;
+	int time = 1;
+	while (!again)
+	{
+		again = true;
+		for (vector<int>::iterator iter1 = nums.begin() + time; iter1 != nums.end(); ++iter1)
+		{
+			vector<int>::iterator iter2 = iter1 - 1;
+			if (*iter1 < *iter2)
+			{
+				swap(*iter1, *iter2);
+				again = false;
+			}
+		}
+	}
+
+	return nums;
+
+}
