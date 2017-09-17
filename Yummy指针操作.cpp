d217e263253e3356bafe322c3d054fc6f495b6bc
suzumiya1215 <1030011936@qq.com>
2017-09-17 12:54:53 +0800

    Yummy指针操作

---
+++ Yummy指针操作.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<iterator>
+#include<string>
+#include<vector>
+#include<set>
+#include<stack>
+#include<algorithm>
+#include <sstream>
+using namespace std;
+
+int main(void)
+{
+	
+	//指针操作
+	char food[] = "Yummy";
+	char *ptr;
+
+	ptr = food + strlen(food);
+	while (--ptr >= food)
+		puts(ptr);
+
+	system("pause");
+	return 0;
+}
